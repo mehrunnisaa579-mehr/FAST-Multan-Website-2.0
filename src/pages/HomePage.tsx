9c9301aa9fbe0c9aff5e0d6e5d1e4f1cef308ecd
Mehrunnisa <mehrunnisaa579@gmail.com>
2026-08-22 10:36:57 +0500

    chore: update homepage section rendering order

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,8 @@
 import Hero from '../components/home/Hero';
 import DirectorMessage from '../components/home/DirectorMessage';
-import EventsAndNews from '../components/home/EventsAndNews';
 import OurSchools from '../components/home/OurSchools';
 import CampusTour from '../components/home/CampusTour';
+import EventsAndNews from '../components/home/EventsAndNews';
 import PhotoGallery from '../components/home/PhotoGallery';
 import WhyChooseUs from '../components/home/WhyChooseUs';
 
@@ -11,9 +11,9 @@ export default function HomePage() {
     <div>
       <Hero />
       <DirectorMessage />
-      <EventsAndNews />
       <OurSchools />
       <CampusTour />
+      <EventsAndNews />
       <PhotoGallery />
       <WhyChooseUs />
     </div>
